2c24d7b7249430139598e404f7a31da2ed452d25
Jason <jayemcee121@gmail.com>
2022-03-22 11:21:15 -0400

    migrating from my personal repository, verified that application is working as expected prior to migration

--- midas-portal/src/app/app.module.ts
+++ midas-portal/src/app/app.module.ts
@@ -1,18 +1,38 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LogInComponent } from './components/log-in/log-in.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularMaterialModule } from './angular-material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LandingComponent } from './components/landing/landing.component';
+import { EdiListComponent } from './components/edi-list/edi-list.component';
+import { RecordsComponent } from './components/records/records.component';
+import { DmpListComponent } from './components/dmp-list/dmp-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogInComponent,
+    LandingComponent,
+    EdiListComponent,
+    RecordsComponent,
+    DmpListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule,
+    FlexLayoutModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
