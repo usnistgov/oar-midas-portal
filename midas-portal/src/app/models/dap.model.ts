9ec07b45d2853b364d3c38a89264261bc3036731
Alex Loembe <alexloembe@Alexs-Laptop.local>
2023-08-24 13:46:51 -0500

    first step creating model

---
+++ midas-portal/src/app/models/dap.model.ts
@@ -0,0 +1,10 @@
+export class dap {
+    name: string;
+    owner: string;
+    file_count: number;
+    title: string;
+    id: string;
+    doi: string;
+    state: string;
+    last_modified: Date;
+}
