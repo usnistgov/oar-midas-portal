db348ce7115b94721865520c87e86b7694311428
Loembe <atl1@pn130437.campus.nist.gov>
2023-10-11 13:06:16 -0400

    config

---
+++ midas-portal/src/app/_helpers/models/dap.model.ts
@@ -0,0 +1,11 @@
+export class Dap{
+    name:string;
+    owner:string;
+    file_associated:string;
+    title:string;
+    type:string;
+    id:string;
+    doi:string;
+    state:string;
+    dateModified:string;
+}
