f7c52bb9abf178270b36b7d65ab62107450b5977
Alex <alexloembe@Alexs-Laptop.fios-router.home>
2024-08-30 08:49:52 -0400

    incase link is bad

--- midas-portal/src/app/components/modals/info/info.component.ts
+++ midas-portal/src/app/components/modals/info/info.component.ts
@@ -11,10 +11,22 @@ import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 })
 export class InfoComponent {
     textContent: SafeHtml = '';
+    placeholder: SafeHtml = '';
     infoURL: string ;
   constructor(private cfgsvc: ConfigurationService,public ref: DynamicDialogRef,
     public config: DynamicDialogConfig,private http: HttpClient,private sanitizer: DomSanitizer) {
     this.config.dismissableMask = true;
+    this.placeholder = this.sanitizer.bypassSecurityTrustHtml(`
+         FROM placeholder
+        <p>Please be advised that the maintenance of this website occurs:</p>
+        <ul>
+          <li><strong>Every Sunday from 9 PM to 10 PM</strong></li>
+          <li><strong>For a full day every two weeks</strong></li>
+        </ul>
+        <p>
+          For any inquiries, please contact <strong>gretchen.greene@nist.gov</strong>.
+        </p>
+      `);
     
   }
 
@@ -30,6 +42,7 @@ export class InfoComponent {
       },
       error => {
         console.error('Error loading text content', error);
+        this.textContent = this.placeholder;
       }
     );
   }
