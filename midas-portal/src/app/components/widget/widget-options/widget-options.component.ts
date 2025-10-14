8460b9473f54733993c7b8374db90fd2dc9d59e8
Alex Loembe <alexloembe@Mac.fios-router.home>
2025-10-14 15:59:28 -0400

    fix stats size widget

--- midas-portal/src/app/components/widget/widget-options/widget-options.component.ts
+++ midas-portal/src/app/components/widget/widget-options/widget-options.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, input, model } from '@angular/core';
+import { Component, inject, input, model, computed } from '@angular/core';
 import { Widget } from '../../../models/dashboard';
 import { DashboardService } from '../../../services/dashboard.service';
 
@@ -11,5 +11,32 @@ export class WidgetOptionsComponent {
   data = input.required<Widget>();
   showOptions = model<boolean>(false);
 
-  store = inject(DashboardService)
+  store = inject(DashboardService);
+
+  // Computed properties to determine if this is a stats widget
+  isStatsWidget = computed(() => {
+    return this.data().label.includes('Stats');
+  });
+
+  // Dynamic column options based on widget type
+  columnOptions = computed(() => {
+    if (this.isStatsWidget()) {
+      // Stats widgets: 1-4 columns
+      return [1, 2, 3];
+    } else {
+      // Table widgets: 2-6 columns  
+      return [2, 3, 4, 5, 6];
+    }
+  });
+
+  // Dynamic row options based on widget type
+  rowOptions = computed(() => {
+    if (this.isStatsWidget()) {
+      // Stats widgets: 1-4 rows
+      return [1, 2, 3];
+    } else {
+      // Table widgets: 3-6 rows
+      return [3, 4, 5, 6];
+    }
+  });
 }
